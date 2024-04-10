a9ebfc01ff9adafa8c7b94169ae3e02c8c3c3303
Gab-Serrano <gab.serrano@duocuc.cl>
2024-04-10 16:57:43 -0400

    SCRIPTS

---
+++ CREA Y LLENA TABLA STUDENT.sql
@@ -0,0 +1,53 @@
+DROP TABLE student;
+
+CREATE TABLE student (
+	ID INT,
+	NAME VARCHAR(50)
+);
+
+-- CON ID
+INSERT INTO student (ID, NAME) VALUES (1, 'Eadith');
+INSERT INTO student (ID, NAME) VALUES (2, 'Gare');
+INSERT INTO student (ID, NAME) VALUES (3, 'Abran');
+INSERT INTO student (ID, NAME) VALUES (4, 'Adelaide');
+INSERT INTO student (ID, NAME) VALUES (5, 'Wiley');
+INSERT INTO student (ID, NAME) VALUES (6, 'Elie');
+INSERT INTO student (ID, NAME) VALUES (7, 'Ara');
+INSERT INTO student (ID, NAME) VALUES (8, 'Donn');
+INSERT INTO student (ID, NAME) VALUES (9, 'Devi');
+INSERT INTO student (ID, NAME) VALUES (10, 'Blancha');
+INSERT INTO student (ID, NAME) VALUES (11, 'Erskine');
+INSERT INTO student (ID, NAME) VALUES (12, 'Roderich');
+INSERT INTO student (ID, NAME) VALUES (13, 'Harwell');
+INSERT INTO student (ID, NAME) VALUES (14, 'Jamima');
+INSERT INTO student (ID, NAME) VALUES (15, 'Midge');
+INSERT INTO student (ID, NAME) VALUES (16, 'Marillin');
+INSERT INTO student (ID, NAME) VALUES (17, 'Quinta');
+INSERT INTO student (ID, NAME) VALUES (18, 'Thane');
+INSERT INTO student (ID, NAME) VALUES (19, 'Ingram');
+INSERT INTO student (ID, NAME) VALUES (20, 'Conney');
+COMMIT;
+
+--SIN ID
+INSERT INTO student ( NAME) VALUES ( 'Eadith');
+INSERT INTO student ( NAME) VALUES ( 'Gare');
+INSERT INTO student ( NAME) VALUES ( 'Abran');
+INSERT INTO student ( NAME) VALUES ( 'Adelaide');
+INSERT INTO student ( NAME) VALUES ( 'Wiley');
+INSERT INTO student ( NAME) VALUES ( 'Elie');
+INSERT INTO student ( NAME) VALUES ( 'Ara');
+INSERT INTO student ( NAME) VALUES ( 'Donn');
+INSERT INTO student ( NAME) VALUES ( 'Devi');
+INSERT INTO student ( NAME) VALUES ( 'Blancha');
+INSERT INTO student ( NAME) VALUES ( 'Erskine');
+INSERT INTO student ( NAME) VALUES ( 'Roderich');
+INSERT INTO student ( NAME) VALUES ( 'Harwell');
+INSERT INTO student ( NAME) VALUES ( 'Jamima');
+INSERT INTO student ( NAME) VALUES ( 'Midge');
+INSERT INTO student ( NAME) VALUES ( 'Marillin');
+INSERT INTO student ( NAME) VALUES ( 'Quinta');
+INSERT INTO student ( NAME) VALUES ( 'Thane');
+INSERT INTO student ( NAME) VALUES ( 'Ingram');
+INSERT INTO student ( NAME) VALUES ( 'Conney');
+
+COMMIT;
